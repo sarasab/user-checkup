e03e5d57bd2c10fd808090c5c0400254900627f7
sara <sara.sabinm@gmail.com>
2021-06-25 14:49:18 +0200

    suite-user-checkup

--- src/App.js
+++ src/App.js
@@ -6,28 +6,37 @@ import UserList from './component/UserList'
 import UserProfile from './component/UseProfile'
 import PageNotFound from './component/PageNotFound'
 
+
 class App extends React.Component {
-  render () {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: {},
+    }
+  }
+
+  render() {
     return (
       <BrowserRouter>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <ul className="navbar-nav">
-            <li className="nav-item"><Link className="nav-link" to="/Login">Login</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/">Login</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/UserList">UserList</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/UserProfile">UserProfile</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/PageNotFound">PageNotFound</Link></li>
-           
-         
+
+
           </ul>
         </nav>
         <Switch>
-          <Route exact path="/" component={Login}/>
-          <Route path="/UserList" component={UserList}/>
-          <Route path="/UserProfile" component={UserProfile}/>
-          <Route path="/PageNotFound" component={PageNotFound}/>
-        
-        </Switch>
-      </BrowserRouter>
+
+          <Route exact path="/" component={props => <Login {...props} />} />
+          <Route path="/UserList" component={UserList} />
+          <Route path="/UserProfile" component={UserProfile} />
+          <Route path="/PageNotFound" component={PageNotFound} />
+          </Switch>
+        </BrowserRouter>
+          
     )
   }
 }
--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,13 +1,45 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
-  render () {
-    return (
-      <h1>
-        Favorites
-      </h1>
-    )
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            userName: '',
+            PassWord: '',
+            userList: [],
+        }
+    }
+    async componentDidMount() {
+        try {
+            const result = await fetch('https://jsonplaceholder.typicode.com/users');
+            console.log(result);
+            const result_JSON = await result.json();
+
+            result_JSON.map(user =>
+                this.setState({
+                    userList: [...this.state.userList, { username: user.username, password: user.address.city }]
+
+                }))
+            console.log(this.state.userList);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    render() {
+
+        return (
+            <div className="row">
+                <div className="col">
+                    <input type="text" className="form-control border-dark " placeholder=" userName" aria-label="First name" />
+                </div>
+                <div className="col">
+                    <input type="text" className="form-control border-dark" placeholder="PassWord" aria-label="Last name" />
+                </div>
+            </div>
+
+        )
+    }
 }
 
 export default Login
